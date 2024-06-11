6089677c9b71b643109f79485d3198f1b8405d6e
qetiproject <ketideveloper@gmail.com>
2024-06-11 14:04:32 +0400

    db

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { PetModule } from '../pet/pet.module';
 import { OwnerModule } from '../owner/owner.module';
 import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PetModule, OwnerModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'pets',
+      username: 'postgres',
+      password: 'root',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    PetModule,
+    OwnerModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
