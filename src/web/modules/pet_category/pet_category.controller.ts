013b93f138fbeb56ac4df8d1ef00f1f5b81de080
qetiproject <ketideveloper@gmail.com>
2024-07-19 10:38:35 +0400

    pet categiry crud

--- src/web/modules/pet_category/pet_category.controller.ts
+++ src/web/modules/pet_category/pet_category.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { PetCategoryService } from './pet_category.service';
 import { AddPetCategoryRequeseDto, PetCategoryResponseDto } from './dto';
 import { ApiTags } from '@nestjs/swagger';
+import { DeleteResponseDto } from '@common/dto';
 
 @ApiTags('PetCategories')
 @Controller('petCategory')
@@ -14,5 +15,22 @@ export class PetCategoryController {
     @Body() addPetCategoryDto: AddPetCategoryRequeseDto,
   ): Promise<PetCategoryResponseDto> {
     return this.petCategoryService.addPetCategoryService(addPetCategoryDto);
+  }
+
+  @Get('/all')
+  petAllCategory(): Promise<PetCategoryResponseDto[]> {
+    return this.petCategoryService.getPetCategoryAllService();
+  }
+
+  @Get('/:id')
+  getPetCategoryDetails(
+    @Param('id') id: string,
+  ): Promise<PetCategoryResponseDto> {
+    return this.petCategoryService.getPetCategoryDetailsService(id);
+  }
+
+  @Delete('/:id')
+  deletePetCategory(@Param('id') id: string): Promise<DeleteResponseDto> {
+    return this.petCategoryService.deletePetCategoryService(id);
   }
 }
