0d95e5ac458b96648a2b0f5d350996194a15425d
qetiproject <ketideveloper@gmail.com>
2024-08-13 19:46:59 +0400

    login

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Pets APP')
     .setDescription('This app is about pets services')
