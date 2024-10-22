23eb359c6cab4c6aeaf25f55169bbe1b993d20fa
Qeti Khetsuriani <ketideveloper@gmail.com>
2024-10-22 13:43:12 +0400

    breeds

--- client/src/app/pages/breed/breed.component.ts
+++ client/src/app/pages/breed/breed.component.ts
@@ -1,14 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { BreedService } from '../services';
 import { Observable } from 'rxjs';
 import { IBreed } from '@app/core/models';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { BreedService } from '../services';
 
 @Component({
   selector: 'app-breed',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './breed.component.html',
   styleUrl: './breed.component.scss'
@@ -17,9 +21,14 @@ export class BreedComponent implements OnInit{
   breedService = inject(BreedService);
 
   breeds$: Observable<IBreed[] | null>
+  breedForm: FormGroup;
 
   constructor() {
     this.breeds$ = this.breedService.breeds$;
+    this.breedForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required])
+    })
   }
   ngOnInit(): void {
     this.getAllBreeds();
@@ -31,4 +40,23 @@ export class BreedComponent implements OnInit{
       error: (error) => {console.error(error)}
     })
   }
+
+  onSubmit(): void {
+    if(this.breedForm.valid) {
+      this.breedService.addBreedService(this.breedForm.value).subscribe({
+        next: () => {
+          this.breedForm.reset()
+        },
+        error: (error) => { console.error(error)}
+      })
+    }
+  }
+
+  deleteBreed(id: string): void {
+    // this.breedService.deleteBreedService(id).subscribe({
+    //   next: () => {},
+    //   error: (error) => {console.error(error)}
+    // })
+  }
+
 }
